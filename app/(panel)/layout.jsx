0fca4953ffd26874e9c5e88b99b7105d407d4005
codayomide <smokayomide@gmail.com>
2024-07-29 18:04:08 +0100

    Styled the side bar

--- app/(panel)/layout.jsx
+++ app/(panel)/layout.jsx
@@ -3,7 +3,7 @@ import SideBar from "@/components/adminPanel/SideBar";
 
 const AdminPanelLayout = ({ children }) => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen text-primaryColor">
       <SideBar />
       <div className="flex-grow flex flex-col">
         <Header />
--- components/adminPanel/SideBar.jsx
+++ components/adminPanel/SideBar.jsx
@@ -1,14 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
 import { BiSolidDashboard } from "react-icons/bi";
+import { FaCaretDown } from "react-icons/fa";
 
 const SideBar = () => {
   return (
-    <aside className="bg-blue-400 w-[13vw]">
-      <div className="flex flex-row justify-between items-center m-4 mb-12 mr-3">
-        <h2 className="uppercase font-semibold text-2xl">menu</h2>
-      </div>
-      <div className="flex flex-col">
-        <div>
+    <aside className="h-full w-[15vw] py-5 flex flex-col justify-between">
+      <div>
+        <div className="text-center mb-14">
+          <h2 className="font-semibold text-xl">SwapT</h2>
+        </div>
+
+        <div className="flex flex-col sidebarLinks">
           <Link href="/dashboard">
             <BiSolidDashboard />
             <span>Dashboard</span>
@@ -42,6 +45,23 @@ const SideBar = () => {
             <span>Dashboard</span>
           </Link>
         </div>
+      </div>
+
+      <div className="flex items-center justify-between w-full px-3">
+        <div className="flex">
+          <Image
+            src={`/avatar.png`}
+            alt="Avatar Image"
+            width={45}
+            height={45}
+            className="mr-2"
+          />
+          <div className="flex flex-col">
+            <h3 className="text-sm font-semibold">Tehilla Nwosu</h3>
+            <text className="text-sm">Manager</text>
+          </div>
+        </div>
+        <FaCaretDown className="text-lg" />
       </div>
     </aside>
   );
