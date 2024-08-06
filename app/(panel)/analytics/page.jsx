209ac1f83d71d7586ccccad28b3e747c8b9d6d0d
codayomide <smokayomide@gmail.com>
2024-08-06 19:01:12 +0100

    Created the layout for the analytics page

--- app/(panel)/analytics/page.jsx
+++ app/(panel)/analytics/page.jsx
@@ -1,9 +1,44 @@
+import { HiOutlineTruck, HiOutlineUserGroup } from "react-icons/hi";
+import { AiOutlineDollarCircle } from "react-icons/ai";
+import { TbAntennaBars5 } from "react-icons/tb";
+
 const AnalyticsPage = () => {
+  const generalStats = [
+    { key: 0, name: "Total Orders", icon: <HiOutlineTruck />, value: "142k" },
+    {
+      key: 1,
+      name: "Total Sales",
+      icon: <AiOutlineDollarCircle />,
+      value: "$214k",
+    },
+    {
+      key: 2,
+      name: "Total Visitors",
+      icon: <HiOutlineUserGroup />,
+      value: "1.5M",
+    },
+    { key: 3, name: "Revenue", icon: <TbAntennaBars5 />, value: "$451k" },
+  ];
+
   return (
-    <>
-      Analytics
-    </>
+    <div className="h-full w-full px-6 py-12 overflow-y-auto">
+      <div className="w-full flex justify-between mb-8">
+        {generalStats.map((item) => (
+          <div
+            key={item.key}
+            className="custom-shadow w-[23%] h-[18vh] rounded-xl"
+          ></div>
+        ))}
+      </div>
+
+      <div className="custom-shadow w-full h-[65vh] mb-8 rounded-xl"></div>
+
+      <div className="w-full flex justify-between">
+        <div className="custom-shadow w-[28%] h-[50vh] rounded-xl"></div>
+        <div className="custom-shadow w-[70%] h-[48vh] rounded-xl"></div>
+      </div>
+    </div>
   );
-}
- 
+};
+
 export default AnalyticsPage;
