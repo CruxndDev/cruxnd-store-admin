2928e10c909f0aba0ef35514472af57961ab3af9
codayomide <smokayomide@gmail.com>
2024-08-09 16:53:59 +0100

    Created a file for arrays

--- app/(panel)/orders/page.jsx
+++ app/(panel)/orders/page.jsx
@@ -2,75 +2,10 @@ import RecentOrdersTable from "@components/adminPanel/orders/RecentOrdersTable";
 import { FaPlus } from "react-icons/fa6";
 import { LuDownloadCloud, LuListFilter, LuSearch } from "react-icons/lu";
 import { CiCalendar } from "react-icons/ci";
+import { orderHistory } from "@utils/arrays";
 
 const OrdersPage = () => {
-  const orderHistory = [
-    {
-      name: "Phoenix Baker",
-      orderId: "#A4092091",
-      price: "$30,021.23",
-      quantity: 1,
-      deliveryDate: "Jan 13, 2024",
-      status: "Cancelled",
-      productDescription:
-        "CLOTHING\n14' by 12' undercover veil\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
-      image: "/assets/avatar.png",
-    },
-    {
-      name: "Natali Craig",
-      orderId: "#A4092091",
-      price: "$10,045.00",
-      quantity: 1,
-      deliveryDate: "Jan 13, 2024",
-      status: "Delivered",
-      productDescription: "SHOES\nSwapTe Pro 3\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
-      image: "/assets/avatar.png",
-    },
-    {
-      name: "Demi Wilkinson",
-      orderId: "#A4092091",
-      price: "$40,132.16",
-      quantity: 1,
-      deliveryDate: "Jan 13, 2024",
-      status: "Delivered",
-      productDescription:
-        "CLOTHING\n14' by 12' undercover veil\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
-      image: "/assets/avatar.png",
-    },
-    {
-      name: "Lana Steiner",
-      orderId: "#A4092091",
-      price: "$22,665.12",
-      quantity: 1,
-      deliveryDate: "Jan 13, 2024",
-      status: "In transit",
-      productDescription:
-        "CLOTHING\n14' by 12' undercover veil\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
-      image: "/assets/avatar.png",
-    },
-    {
-      name: "Candice Wu",
-      orderId: "#A4092091",
-      price: "$18,221.30",
-      quantity: 1,
-      deliveryDate: "Jan 12, 2024",
-      status: "Delivered",
-      productDescription:
-        "CLOTHING\n14' by 12' undercover veil\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
-      image: "/assets/avatar.png",
-    },
-    {
-      name: "Lana Steiner",
-      orderId: "#A4092091",
-      price: "$24,118.18",
-      quantity: 1,
-      deliveryDate: "Jan 12, 2024",
-      status: "Delivered",
-      productDescription:
-        "CLOTHING\n14' by 12' undercover veil\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
-      image: "/assets/avatar.png",
-    },
-  ];
+  const OrderHistoryArray = orderHistory;
 
   return (
     <div className="h-full w-full flex flex-col px-6 py-12 overflow-y-auto">
@@ -124,7 +59,7 @@ const OrdersPage = () => {
           </div>
         </div>
 
-        <RecentOrdersTable data={orderHistory} />
+        <RecentOrdersTable data={OrderHistoryArray} />
       </div>
     </div>
   );
---
+++ utils/arrays.js
@@ -0,0 +1,67 @@
+export const orderHistory = [
+  {
+    name: "Phoenix Baker",
+    orderId: "#A4092091",
+    price: "$30,021.23",
+    quantity: 1,
+    deliveryDate: "Jan 13, 2024",
+    status: "Cancelled",
+    productDescription:
+      "CLOTHING\n14' by 12' undercover veil\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
+    image: "/assets/avatar.png",
+  },
+  {
+    name: "Natali Craig",
+    orderId: "#A4092091",
+    price: "$10,045.00",
+    quantity: 1,
+    deliveryDate: "Jan 13, 2024",
+    status: "Delivered",
+    productDescription: "SHOES\nSwapTe Pro 3\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
+    image: "/assets/avatar.png",
+  },
+  {
+    name: "Demi Wilkinson",
+    orderId: "#A4092091",
+    price: "$40,132.16",
+    quantity: 1,
+    deliveryDate: "Jan 13, 2024",
+    status: "Delivered",
+    productDescription:
+      "CLOTHING\n14' by 12' undercover veil\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
+    image: "/assets/avatar.png",
+  },
+  {
+    name: "Lana Steiner",
+    orderId: "#A4092091",
+    price: "$22,665.12",
+    quantity: 1,
+    deliveryDate: "Jan 13, 2024",
+    status: "In transit",
+    productDescription:
+      "CLOTHING\n14' by 12' undercover veil\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
+    image: "/assets/avatar.png",
+  },
+  {
+    name: "Candice Wu",
+    orderId: "#A4092091",
+    price: "$18,221.30",
+    quantity: 1,
+    deliveryDate: "Jan 12, 2024",
+    status: "Delivered",
+    productDescription:
+      "CLOTHING\n14' by 12' undercover veil\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
+    image: "/assets/avatar.png",
+  },
+  {
+    name: "Lana Steiner",
+    orderId: "#A4092091",
+    price: "$24,118.18",
+    quantity: 1,
+    deliveryDate: "Jan 12, 2024",
+    status: "Delivered",
+    productDescription:
+      "CLOTHING\n14' by 12' undercover veil\n★ ★ ★ ★ ☆ (4.6) 281 ratings",
+    image: "/assets/avatar.png",
+  },
+];
